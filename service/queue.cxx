e9395fe603bd04da87e88a9d0431e49672db58aa
Max Dark <maxim.dark@gmail.com>
2020-12-09 22:25:25 +0300

    add minimal Queue realisation

--- service/queue.cxx
+++ service/queue.cxx
@@ -3,3 +3,34 @@
 //
 
 #include "queue.hxx"
+
+void Queue::push(Queue::value_type value)
+{
+    std::lock_guard lock{guard};
+
+    queue.emplace(std::move(value));
+    event.notify_one();
+}
+
+bool Queue::empty() const
+{
+    std::lock_guard lock{guard};
+    return queue.empty();
+}
+
+bool Queue::try_pop(Queue::value_type& value, Queue::timeout millisec)
+{
+    std::unique_lock lock{guard};
+
+    auto state = event.wait_for(lock, millisec, [this]{
+        return !queue.empty();
+    });
+
+    if (state)
+    {
+        value = std::move(queue.front());
+        queue.pop();
+    }
+
+    return state;
+}
--- service/queue.hxx
+++ service/queue.hxx
@@ -5,9 +5,30 @@
 #ifndef STREAMED_QUEUE_HXX
 #define STREAMED_QUEUE_HXX
 
+#include <queue>
+#include <string>
+
+#include <chrono>
+
+#include <mutex>
+#include <condition_variable>
+
 class Queue
 {
+public:
+    using value_type = std::string;
+    using timeout = std::chrono::milliseconds;
 
+    void push(value_type value);
+
+    bool empty() const;
+
+    bool try_pop(value_type& value, timeout millisec);
+private:
+    std::queue<value_type> queue;
+
+    mutable std::mutex guard;
+    std::condition_variable event;
 };
 
 #endif //STREAMED_QUEUE_HXX
