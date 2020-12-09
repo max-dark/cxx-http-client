c5019a35e45b5e875515f594a0a5d9a24bb49c43
Max Dark <maxim.dark@gmail.com>
2020-12-09 22:02:16 +0300

    add Queue stub

---
+++ service/queue.cxx
@@ -0,0 +1,5 @@
+//
+// Created by max on 09.12.2020.
+//
+
+#include "queue.hxx"
---
+++ service/queue.hxx
@@ -0,0 +1,13 @@
+//
+// Created by max on 09.12.2020.
+//
+
+#ifndef STREAMED_QUEUE_HXX
+#define STREAMED_QUEUE_HXX
+
+class Queue
+{
+
+};
+
+#endif //STREAMED_QUEUE_HXX
